0d8de13a881dc8e10cfca9f53001312510d6f791
ccrisgar <ccrisgar@gmail.com>
2022-09-06 18:59:49 -0500

    design registry employee and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeRegistryComponent } from './employee-registry/employee-registry.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegistryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-registry/employee-registry.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-registry',
+  templateUrl: './employee-registry.component.html',
+  styleUrls: ['./employee-registry.component.scss']
+})
+export class EmployeeRegistryComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
